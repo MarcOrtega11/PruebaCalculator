a84e7b2e2564b776d1bf97a4434cd995d0d30d7b
maorcomaop <marcomaop@gmail.com>
2019-06-17 16:39:59 -0400

    Added a comment.

--- src/main/java/com/finrestcalc/variables/VariablesDeCalc.java
+++ src/main/java/com/finrestcalc/variables/VariablesDeCalc.java
@@ -6,7 +6,7 @@ public class VariablesDeCalc {
 	private Double number2;
 	private String operation;
 	private Double result;
-
+	//Test Comment
 	
 	  public String getOperation() { return operation; }
 	  
